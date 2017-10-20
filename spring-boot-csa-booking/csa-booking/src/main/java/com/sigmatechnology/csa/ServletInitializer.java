8493e56f283a3ea1df2ca56be31469e83f6d05ae
LucianaHaugen <lucianahaugen@gmail.com>
2017-10-20 11:18:57 +0200

    booking back to spring-boot .jar och a few new REST APIs

---
+++ spring-boot-csa-booking/csa-booking/src/main/java/com/sigmatechnology/csa/ServletInitializer.java
@@ -0,0 +1,18 @@
+package com.sigmatechnology.csa;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by lucianahaugen on 10/10/17.
+ * To be used when creating a WAR that is going to be deployed on a AWS Tomcat enviroment.
+ */
+/*public class ServletInitializer extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(CsaApplication.class);
+    }
+}*/
+
